fb1cd5e77fdb1d2800dbff26f4526e860ed64f3e
Evgeniy Shvecov <evgeshvecov@yandex.ru>
2015-02-06 17:53:35 +0200

    06.02

--- RestFull/views/library.js
+++ RestFull/views/library.js
@@ -5,6 +5,18 @@ var app = app||{};
 app.LibraryView = Backbone.View.extend({
    el:'#books',
    initialize:function(initialBooks){
-
-   }
+        this.collection = new app.Library(initialBooks);
+       this.render();
+   },
+    render:function() {
+        this.collection.each(function(el){
+            this.renderBook(el);
+        },this);
+    },
+    renderBook:function(el){
+        var bookView = new app.BookView({
+            model:el
+        });
+        this.$el.append(bookView.render().el)
+    }
 });
